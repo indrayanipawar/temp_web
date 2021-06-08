53370da76c83b7b369a854a738cd6a19d104a0fe
sau22rane <scrane22301@gmail.com>
2021-06-08 18:45:41 +0530

    config file added

--- setup.sh
+++ setup.sh
@@ -1,6 +1,6 @@
 sudo apt-get update
-sudo apt-get install npm
-sudo apt-get install pip3
+sudo apt-get install npm -y
+sudo apt-get install python3-pip -y
 sudo pip3 install -U pip
-sudo pip3 install -r temp_web/Server/requirements.txt
-sudo apt install python3-opencv
+sudo pip3 install -r Server/requirements.txt
+sudo apt install python3-opencv -y
