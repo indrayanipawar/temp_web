aff137b788f79bdb424f66f9ddaac083c3e4be11
sau22rane <scrane22301@gmail.com>
2021-06-08 18:34:18 +0530

    config file added

--- setup.sh
+++ setup.sh
@@ -1,6 +1,5 @@
 sudo apt-get update
 sudo apt-get install npm
-git clone https://github.com/sau22rane/temp_web.git
 sudo apt-get install pip3
 sudo pip3 install -U pip
 sudo pip3 install -r temp_web/Server/requirements.txt
